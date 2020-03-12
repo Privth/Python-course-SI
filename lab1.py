5dea3a217ab658448d64cde5e52f54e98e10a637
Wojciech Król <wojciechkrol97@gmail.com>
2020-03-12 16:05:56 +0100

    add bubble sort algorithm

--- lab1.py
+++ lab1.py
@@ -16,4 +16,20 @@ def generate_random_numbers(length, minimum, maximum):
     return array
 
 
+def bubble_sort(array):
+    if len(array) == 0:
+        return print('array length can not be equal to 0')
 
+    length = len(array)
+    for i in range(length):
+        swapped = False
+
+        for j in range(0, length - i - 1):
+            if array[j] > array[j + 1]:
+                array[j], array[j + 1] = array[j + 1], array[j]
+                swapped = True
+
+        if not swapped:
+            break
+
+        return array
